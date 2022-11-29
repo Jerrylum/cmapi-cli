5fe19256dbdf59f04830aa2c1d386e38a64eb967
Jerrylum <serverofjerry@gmail.com>
2022-11-30 03:27:38 +0800

    :bookmark: Version 0.1.2

--- main.go
+++ main.go
@@ -735,7 +735,7 @@ Options:
     -s,  --slot <SLOT>          Upload the binary to a specified program slot
                                 in the brain. [default: 1, range: 1-8]
 
-Version: 0.1.1`
+Version: 0.1.2`
 
 // Returns true if the given label is valid
 // No side effect
